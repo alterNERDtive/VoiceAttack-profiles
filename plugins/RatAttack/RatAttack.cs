14092bf174a6501d742cce3bffc82079a10219fe
alterNERDtive <nerd@alternerd.tv>
2021-02-16 12:08:44 +0100

    RatAttack: removed “Valid system name” thing from the regex
    Mecha now gives an estimation based on the sector name!

--- plugins/RatAttack/RatAttack.cs
+++ plugins/RatAttack/RatAttack.cs
@@ -18,7 +18,7 @@ namespace RatAttack
         private static alterNERDtive.util.PipeServer<Ratsignal>? ratsignalPipe;
 
         private static readonly Regex RatsignalRegex = new Regex(
-            @"^RATSIGNAL - CMDR (?<cmdr>.+) - Reported System: (None|""(?<system>.+)""(\x0314)? \((?<systemInfo>.*([0-9,\.]+ LY from [a-zA-Z0-9\s]+|Not found in galaxy database|Invalid system name|Valid system name)?)\)(\x03)?(?<permit> \x037\(((?<permitName>.*) )?Permit Required\)\x03)?) - Platform: \x03(6|3|12)(?<platform>(PC|Xbox|Playstation))\x03 - O2: (\x034)?(?<oxygen>(NOT )?OK)\x03? - Language: .+ \(\x02Case #(?<number>\d+)\x02\) \((PC|XB|PS)_SIGNAL\)\v*$"
+            @"^RATSIGNAL - CMDR (?<cmdr>.+) - Reported System: (None|""(?<system>.+)""(\x0314)? \((?<systemInfo>.*([0-9,\.]+ LY from [a-zA-Z0-9\s]+|Not found in galaxy database|Invalid system name)?)\)(\x03)?(?<permit> \x037\(((?<permitName>.*) )?Permit Required\)\x03)?) - Platform: \x03(6|3|12)(?<platform>(PC|Xbox|Playstation))\x03 - O2: (\x034)?(?<oxygen>(NOT )?OK)\x03? - Language: .+ \(\x02Case #(?<number>\d+)\x02\) \((PC|XB|PS)_SIGNAL\)\v*$"
             );
 
         private static VoiceAttackLog Log
